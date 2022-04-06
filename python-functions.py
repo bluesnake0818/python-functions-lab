87ae444d5464b61318d72c8897d96607279c5f42
bluesnake0818 <raidient87@gmail.com>
2022-04-06 04:56:20 -0400

    lab complete

--- python-functions.py
+++ python-functions.py
@@ -6,14 +6,14 @@ def sum_to (n):
     counter -= 1
   return sum
     
-print("Problem 1")
+print("\nProblem 1")
 print(sum_to(6))  # returns 21
 print(sum_to(10)) # returns 55)
 
 def largest (listNum):
   return max(listNum)
 
-print("Problem 2")
+print("\nProblem 2")
 print(largest([1, 2, 3, 4, 0]))  # returns 4
 print(largest([10, 4, 2, 231, 91, 54]))  # returns 231
 
@@ -21,7 +21,7 @@ def occurrences (str1, str2):
   count = str1.count(str2)
   return count
 
-print("Problem 3")
+print("\nProblem 3")
 print(occurrences('fleep floop', 'e'))   # returns 2
 print(occurrences('fleep floop', 'p'))   # returns 2
 print(occurrences('fleep floop', 'ee'))  # returns 1
@@ -33,6 +33,7 @@ def product(*args):
     product = product * arg
   return product
 
+print("\nProblem 4")
 print(product(-1, 4)) # returns -4
 print(product(2, 5, 5)) # returns 50
 print(product(4, 0.5, 5)) # returns 10.0
