6f4c83e85aeb25d9add6095f690bf80a7ba20ea8
bluesnake0818 <raidient87@gmail.com>
2022-04-06 04:27:25 -0400

    solve problem 2

--- python-functions.py
+++ python-functions.py
@@ -3,10 +3,15 @@ def sum_to (n):
   sum = 0
   while counter > 0:
     sum = sum + counter 
-    counter = counter - 1
+    counter -= 1
   return sum
     
 
 print(sum_to(6))  # returns 21
 print(sum_to(10)) # returns 55)
-# print(sum_to(-1))
+
+def largest (listNum):
+  return max(listNum)
+
+print(largest([1, 2, 3, 4, 0]))  # returns 4
+print(largest([10, 4, 2, 231, 91, 54]))  # returns 231
